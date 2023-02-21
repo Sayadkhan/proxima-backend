926dd7f9860341ac74ff0e32208077d38688f4a5
Sayadkhan <abdullahalsayad03@gmail.com>
2023-02-21 21:09:04 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+
+// express app
+
+const app = express();
+
+//port
+const port = process.env.PORT || 4000;
+
+// listen for request
+
+app.listen(port, () => {
+  console.log(`listening on  port ${port}`);
+});
