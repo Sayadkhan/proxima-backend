e96238e55fc6e277b55ff76f06447638fdbded2e
Sayadkhan <abdullahalsayad03@gmail.com>
2023-02-22 01:02:03 +0600

    mongo connected

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const projectRoutes = require("./routes/projectRoute");
+const mongoose = require("mongoose");
 
 // express app
 
@@ -19,8 +20,17 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/projects", projectRoutes);
 
-// listen for request
+// mongodb
+mongoose.set("strictQuery", false); //optional
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`listening on  port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(port, () => {
-  console.log(`listening on  port ${port}`);
-});
+// listen for request
